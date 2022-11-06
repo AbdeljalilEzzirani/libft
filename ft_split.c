fdd59c33a9b42e9ebb34c547fcdd6d697ba50fc1
Abde-Ljalil Ez-Zirani <abez-zir@student.1337.ma>
2022-11-06 22:13:32 +0100

    06/11/2022

--- ft_split.c
+++ ft_split.c
@@ -6,46 +6,51 @@
 /*   By: abez-zir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 16:49:20 by abez-zir          #+#    #+#             */
-/*   Updated: 2022/11/02 22:13:08 by abez-zir         ###   ########.fr       */
+/*   Updated: 2022/11/06 22:12:23 by abez-zir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char function1()
-{
-	//heeeeeeeeeeeere
-}
-
-char	function2()
-{
-	//this function
-}
-
-char	**ft_split(char const *s, char c)
+char **ft_split(char const *s, char c)
 {
 	int				i;
-	char			**p;
-	//char			*ptr;
+	int				j;
+	int				len;
+	char			**tab;
+	char			*cas;
 
-	p = (char *) malloc ((lent + 1) * sizeof(char));
-	if (p == NULL)
-		return (NULL);
-	p = ft_strtrim(s, c);
 	i = 0;
-	while (p[i])
+	while (s[i])
 	{
-		ptr = (char *) malloc ((lent + 1) * sizeof(char));
-		if (ptr == NULL)
-			return (NULL);
-		ptr = ft_substr(s1, i, lent);
-		if ()
+		while (s[i] != c)
+		{
+			j = i;
+			++tab;
+			while (s[j] != c)
+				j++;
+			len = j - i;
+			cas = ft_substr(s, i, len);
+			tab = (char **) malloc (sizeof(char [i][len + 1]));
+			ft_strlcpy((char *)s, tab[i], len + 1);
+			i = j;
+		}
+		i++;
 	}
+	return (tab);
 }
 
 int	main ()
 {
 	char src[]="__hello__world_how_are_you___";
-	char ch="_";
-	ft_split(src, ch);
+	char			**p;
+	int				i;
+
+	i = 0;
+	p = ft_split(src, '_');
+	while (p[i])
+	{
+		printf ("%s\n", p[i]);
+		i++;
+	}
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,22 +6,18 @@
 /*   By: abez-zir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 15:22:32 by abez-zir          #+#    #+#             */
-/*   Updated: 2022/10/20 20:58:04 by abez-zir         ###   ########.fr       */
+/*   Updated: 2022/11/06 19:06:37 by abez-zir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
-
-size_t	ft_strlen(const char *s);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t				i;
 	size_t				n;
 
-	n = strlen(src);
+	n = ft_strlen(src);
 	if (dstsize == 0)
 		return (n);
 	i = 0;
